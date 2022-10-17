9c6acde50cb1e4a8e5ca534abf331619105ef36a
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-17 20:25:05 +0500

    init skeleton

---
+++ main.py
@@ -0,0 +1,23 @@
+import telegram
+import pytube
+
+
+def start(update, context):
+    """Sends description about bot
+    """
+    pass
+
+def url(update, context):
+    """ Gets url and sends message about url
+    and resolution inline buttons
+    """
+    pass
+
+def url_inline_button(update, context):
+    """ When inline button pressed send downloads video with user asked resolution
+    and sends video to user also save vidoe id to use other user ask same url video
+    """
+    pass
+
+
+# handlers here
