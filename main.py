91201e83526c0d3cb462ad0e9796a6a6941e3e4b
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-18 00:08:00 +0500

    starting point

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
-import telegram
-import pytube
+from telegram.ext import (
+    Updater, 
+    MessageHandler, 
+    CommandHandler,
+    CallbackQueryHandler,
+    Filters,
+    CallbackContext,
+)
+from telegram import (
+    Update, 
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup,
+)
+from pytube import YouTube, Playlist
 
+TOKEN = '5128554563:AAGXrWFZ8YKt0lSbFpr6X1xo4P14X-L5NjE'
 
-def start(update, context):
+def start(update: Update, context: CallbackContext):
     """Sends description about bot
     """
-    pass
+    update.message.reply_text('hi')
 
-def url(update, context):
+def url(update: Update, context: CallbackContext):
     """ Gets url and sends message about url
     and resolution inline buttons
     """
-    pass
+    text = update.message.text
+    yt = YouTube(text)
+    streams = yt.streams.filter(progressive=True)
 
-def url_inline_button(update, context):
+    resolutions = []
+    for stream in streams:
+        resolutions.append(InlineKeyboardButton(stream.resolution, callback_data=stream.resolution))
+    
+    reply = InlineKeyboardMarkup([
+        [i for i in resolutions[:3]]
+    ])
+    
+    update.message.reply_text(text, reply_markup=reply)
+
+def url_inline_button(update: Update, context: CallbackContext):
     """ When inline button pressed send downloads video with user asked resolution
     and sends video to user also save vidoe id to use other user ask same url video
     """
-    pass
+    query = update.callback_query
+    text = query.message.text
+    yt = YouTube(text).streams.get_by_resolution(query.data)
+    yt.download()
+    query.answer('done')
 
 
 # handlers here
+updater = Updater(TOKEN)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler('start', start))
+dp.add_handler(MessageHandler(Filters.entity('url'), url))
+dp.add_handler(CallbackQueryHandler(url_inline_button))
+
+updater.start_polling()
+updater.idle()
